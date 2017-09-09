ef09602dc1bba2671b7d8014ce425f57f2771c66
Anton Dzodzikov <anton.dzodzikov@gmail.com>
2017-09-09 20:08:09 +1000

    Add basic layout and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  styleUrls: ['styles.css'],
+  template: `      
+      <div class="board">
+          <div class="column">
+              <div class="column-title">To Do</div>
+              <div class="column-body">tickets</div>
+          </div>
+          <div class="column">
+              <div class="column-title">In Progress</div>
+              <div class="column-body">tickets</div>
+          </div>
+          <div class="column">
+              <div class="column-title">Complete</div>
+              <div class="column-body">tickets</div>
+          </div>
+      </div>
+  `
 })
 export class AppComponent  { name = 'Angular'; }
